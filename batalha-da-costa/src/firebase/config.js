7099abeefbf2b2549c133321c7acbf889ba071ee
Carlos de Lima Junior <cj.moyses@gmail.com>
2025-04-15 20:34:32 -0300

    feat: adiciona configuração de acesso ao firebase

---
+++ batalha-da-costa/src/firebase/config.js
@@ -0,0 +1,22 @@
+import * as admin from "firebase-admin";
+import "dotenv/config";
+
+const serviceAccount = {
+  type: import.meta.env.VITE_FIREBASE_TYPE,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  private_key_id: import.meta.env.VITE_FIREBASE_PRIVATE_KEY_ID,
+  privateKey: import.meta.env.VITE_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  clientEmail: import.meta.env.VITE_FIREBASE_CLIENT_EMAIL,
+  client_id: import.meta.env.VITE_FIREBASE_CLIENT_ID,
+  auth_uri: import.meta.env.VITE_FIREBASE_AUTH_URI,
+  token_uri: import.meta.env.VITE_FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: import.meta.env.VITE_FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: import.meta.env.VITE_FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: import.meta.env.VITE_FIREBASE_UNIVERSE_DOMAIN,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export const firebase = admin.firestore();
